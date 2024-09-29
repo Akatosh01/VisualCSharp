a5e606f7fc530b578051bcdb912eab3f8fc9f2ad
Akatosh01 <samzx03@hotmail.com>
2024-09-29 12:45:31 -0400

    first commit

---
+++ holamundo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace holamundo
+{
+    class holaMundo
+    {
+        public void Mostrar()
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            holaMundo HW = new holaMundo();
+            HW.Mostrar();
+            Console.ReadKey();           
+
+        }
+    }
+}
